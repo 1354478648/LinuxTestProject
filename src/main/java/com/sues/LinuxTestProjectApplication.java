4b1c0207da24ab3d9dca079a303b8ee1217d84ee
Downey <1354478648@qq.com>
2023-05-04 19:58:36 +0800

    Initial commit

---
+++ src/main/java/com/sues/LinuxTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinuxTestProjectApplication.class, args);
+    }
+
+}
